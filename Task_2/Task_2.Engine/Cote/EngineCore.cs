d00003a52501a83210498b90154be0a4930e735b
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-11-13 09:48:42 +0200

    Initial commit with task 2

---
+++ Task_2/Task_2.Engine/Cote/EngineCore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2.Engine.Core
+{
+    internal class EngineCore
+    {
+        private int calculateNextYear;
+
+        public EngineCore(int year)
+        {
+            this.calculateNextYear = year;
+        }
+
+        internal int Init()
+        {  
+            return CalculateYear();
+        }
+
+        private int CalculateYear()
+        {
+            while (true)
+            {
+                string nextYearToString = this.calculateNextYear.ToString();
+                if (!CheckNextYear(nextYearToString))
+                {
+                    this.calculateNextYear += 1;
+                }
+                else
+                {
+                    return this.calculateNextYear;
+                }
+
+            }
+        }
+
+        private bool CheckNextYear(string nextYearToString)
+        {
+            for (int i = 0; i < nextYearToString.Length; i++)
+            {
+                int currentNumber = int.Parse(nextYearToString[i].ToString());
+                for (int j = i + 1; j < nextYearToString.Length; j++)
+                {
+                    int checkingYear = int.Parse(nextYearToString[j].ToString());
+                    if (checkingYear == currentNumber)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Task_2/Task_2/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using InputReader;
+using Interfaces;
+using MainEngine.Interfaces;
+using OutputWriter;
+using Task_2.Engine.Core;
+
+namespace Task_2
+{
+    internal class StartUp
+    {
+        internal static void Main(string[] args)
+        {
+            IReadable reader = new Input();
+            IWriteable write = new Display();
+            IDrawable engine = new EngineInit(reader.GetNumber());
+            write.Print(engine.Run());
+        }
+    }
+}
