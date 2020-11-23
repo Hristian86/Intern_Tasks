214bf5c59be6dd0af83e5b6fea31ba85fee9b6bd
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-11-23 13:25:42 +0200

    Solution with array of digits instead of bool

---
+++ Session_2/Task_1/Task_1.swift
@@ -0,0 +1,77 @@
+protocol IMain {
+    func Main() -> Void
+}
+
+public class StartUp: IMain {
+
+// You have to search all numbers from inclusive 1 to inclusive a given number x, that have the given digit d in it.
+// The value of d will always be 0 - 9.
+// The value of x will always be greater than 0.
+// You have to return as an array
+// the count of these numbers,
+// their sum
+// and their product.
+
+// Evgeni Milev12:00 PM
+// Given a number , Return _The Maximum number _ could be formed from the digits of the number given .
+// Only Natural numbers passed to the function , numbers Contain digits [0:9] inclusive
+
+// Digit Duplications could occur , So also consider it when forming the Largest
+// As 321 is _The Maximum number _ could be formed from the digits of the number *213*** .
+// Evgeni Milev12:01 PM
+// As 9873 is _The Maximum number _ could be formed from the digits of the number *7389*** .
+// maxNumber (213) ==> return (321)
+// Velina Tsivneva12:18 PM
+// .sorted(by:>) 
+// Evgeni Milev12:18 PM
+// let reversed1 = Array(numbers.sorted().reversed())
+// Evgeni Milev12:26 PM
+// for (index, element) in list.enumerated() 
+
+    public func Main() -> Void {
+        // Start to code here.
+        let display = Calculate(1864930450);
+        let resultNumber = Int(display) ?? 0;
+        print(resultNumber);
+    }
+
+    
+    func Calculate(_ number: Int) -> String {
+        var tempNumber = number;
+        var numbers = [0,0,0,0,0,0,0,0,0,0];
+
+        while tempNumber > 0 {
+            let lastNum = tempNumber % 10;
+            numbers[lastNum] += 1;
+
+            tempNumber /= 10;
+        }
+        
+        var toBeDisplayed = Array<Int>();
+        // String result
+        var displs: String = "";
+        // Reversed numbers.
+        for i in 0..<numbers.count {
+            // print(numbers[numbers.count - i - 1]);
+            
+            if numbers[numbers.count - i - 1] > 0 {
+                var res = numbers[numbers.count - i - 1] * (numbers.count - i - 1)
+                var countOfNumbers = numbers[numbers.count - i - 1];
+                if countOfNumbers > 1 {
+                    while countOfNumbers != 0 {
+                        toBeDisplayed.append(res);
+                        displs += String(res);
+                        countOfNumbers -= 1;
+                    }
+                } else {
+                    displs += String(res);
+                    toBeDisplayed.append(res);
+                }
+            }
+        }
+        return displs;
+    }    
+}
+
+var startUp = StartUp();
+startUp.Main();
